68fee7ab216e0be8f613bb7b0feca9cebc30950e
alessandroamormino <amormino.alessandro97@gmail.com>
2023-03-15 14:14:09 +0100

    feat: milestone 1 done

--- js/script.js
+++ js/script.js
@@ -3,7 +3,39 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      todos : [
+        {
+            text: 'Comprare il latte',
+            done: false,
+        },
+        {
+            text: 'Comprare il pane',
+            done: false,
+        },
+        {
+            text: 'Pulire casa',
+            done: false,
+        },
+        {
+            text: 'Fare benzina',
+            done: false,
+        },
+      ], 
+      styleToDo: {
+        color: 'red',
+        textDecoration: 'line-through',
+      },
     }
-  }
+  }, 
+
+  methods: {
+    isDone(toDoIndex){
+        if(this.todos[toDoIndex].done){
+            this.todos[toDoIndex].done = false;
+        } else{
+            this.todos[toDoIndex].done = true;
+        }
+    }
+  },
+
 }).mount('#app')
